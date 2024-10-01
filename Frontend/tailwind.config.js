30d2dc566715d8a5cae0de733fb12b488f3e11bc
gaurav bisht <gauravengg.bisht@gmail.com>
2024-10-01 23:16:23 +0530

    Home page is completed

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -7,7 +7,11 @@ export default {
   theme: {
     extend: {
       colors:{
-        'primary': '#5f6fff'
+        'primary': '#5f6fff',
+
+      },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fill, minmax(200px,1fr))'
       }
     },
   },
